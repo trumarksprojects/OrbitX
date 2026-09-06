a9884fb833eb9814144a37427373d0fbf8bff2e0
trumarksprojects <danieloriome@gmail.com>
2026-09-06 02:08:08 +0100

    Add Prettier configuration for consistent code formatting

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'always',
+  printWidth: 100,
+};
+
+export default config;
